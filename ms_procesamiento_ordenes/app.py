09a193587fa9f292b5d535c1605cad26babfb96f
Santiago <santirua3@gmail.com>
2025-02-17 17:38:57 -0500

    Update dependencies and modify order model validation; comment out Kafka producer code

--- ms_procesamiento_ordenes/app.py
+++ ms_procesamiento_ordenes/app.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from kafka import KafkaProducer
+# from kafka import KafkaProducer
 import json
 
 app = FastAPI()
 
 class Order(BaseModel):
-    order_type: str = Field(..., regex="^(venta|compra)$")  # "venta" o "compra"
+    order_type: str = Field(..., pattern="^(venta|compra)$")  # "venta" o "compra"
     asset_type: str
     amount_in_usd: float = Field(..., gt=0)
 
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+# producer = KafkaProducer(
+#     bootstrap_servers='localhost:9092',
+#     value_serializer=lambda v: json.dumps(v).encode('utf-8')
+# )
 
 @app.get("/health")
 def health():
@@ -27,6 +27,8 @@ def create_order(order: Order):
         raise HTTPException(status_code=400, detail="Amount must be greater than 0")
 
     topic = 'orders_purchase' if order.order_type == 'compra' else 'orders_selling'
-    producer.send(topic, order.dict())
-    producer.flush()
+    print(f"Order sent to topic {topic}: {order}")
+    
+    # producer.send(topic, order.dict())
+    # producer.flush()
     return {"message": "Order sent to Kafka", "order": order.dict()}
