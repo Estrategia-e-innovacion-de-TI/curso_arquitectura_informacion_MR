fd85b7c51a9b32e11cd5e5299842afa03d4cf7bd
Santiago <santirua3@gmail.com>
2025-02-17 17:49:43 -0500

    Add AWS subnets data source to Terraform configuration

--- deployment/data_sources.tf
+++ deployment/data_sources.tf
@@ -2,3 +2,10 @@
 data "aws_ecs_cluster" "existing_ecs_cluster_innovacion" {
   cluster_name = "ecs-arquitectura-innovacion-sbx"
 }
+
+data "aws_subnets" "default" {
+    filter{
+        name = "vpc-id"
+        values = [aws_default_vpc.default.id]
+    }
+}
