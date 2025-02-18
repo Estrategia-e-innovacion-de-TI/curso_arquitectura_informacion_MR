efa58c30d9660aa296cf5b667c9a4707be7d6c5f
Santiago <santirua3@gmail.com>
2025-02-18 17:50:25 -0500

    Add Terraform variables for EC2 instance configuration

---
+++ deployment/variables.tf
@@ -0,0 +1,29 @@
+variable "image_ec2" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  type        = string
+}
+
+variable "ssh_key" {
+  description = "SSH key name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "AWS availability zone"
+  type        = string
+}
+
+variable "user_data_file" {
+  description = "User data file for instance"
+  type        = string
+}
