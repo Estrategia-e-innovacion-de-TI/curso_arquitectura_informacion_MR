0a33870496297ccd399502ee85a3753116b6037c
Santiago <santirua3@gmail.com>
2025-02-18 17:48:52 -0500

    Add Terraform configuration for AWS EC2 instance to run JMeter tests

---
+++ deployment/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "jmeter_orders"{
+  ami = var.image_ec2
+  instance_type = var.instance_type
+  key_name = var.ssh_key
+  subnet_id = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.jmeter_instance_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_role_profile.name
+  user_data = file(var.user_data_file)
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "arquitecturaSoftwareJMeterOrderInstance"
+  }
+
+  root_block_device {
+      volume_size = 2000
+  }
+}
+
+output "instance_public_ips" {
+  value = aws_instance.jmeter_orders.public_ip
+}
