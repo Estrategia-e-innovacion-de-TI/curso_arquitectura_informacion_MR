81506b09da7a77dd619d5fdfef93e6f821b35966
Santiago <santirua3@gmail.com>
2025-02-19 11:02:15 -0500

    Update user_data.sh to install Java 21, add unzip utility, and clone the repository

--- deployment/user_data.sh
+++ deployment/user_data.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 echo $HOME
 
+# Update
+sudo apt update -y
+
 # Install Java 11
-sudo apt install openjdk-11-jre-headless -y
-sudo apt install openjdk-11-jdk-headless -y
+sudo apt install openjdk-21-jre-headless -y
+sudo apt install openjdk-21-jdk-headless -y
 
 # Download jmeter
 wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.6.3.zip
 
 # Unzip jmeter
-unzip apache-jmeter-5.6.3.zip -d /opt/jmeter-5.6.3
+sudo apt install unzip -y
+sudo unzip apache-jmeter-5.6.3.zip -d /opt/jmeter-5.6.3
 
 # Add jmeter to PATH
 echo "export PATH=$PATH:/opt/jmeter-5.6.3/apache-jmeter-5.6.3/bin" >> ~/.bashrc
 source ~/.bashrc
+
+# clone the repository
+git clone https://github.com/Estrategia-e-innovacion-de-TI/curso_arquitectura_informacion_MR.git
